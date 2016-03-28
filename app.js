b6fa926f9ff35bcae8d69d3e268961bc82c5a41e
Amberact <Amberact@users.noreply.github.com>
2016-03-28 17:40:03 +0800

    Added files via upload

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var mime=require('config/mime');
+const globby = require('globby');
+var xattr = require('fs-xattr');
+var MTObj = require('middleware/memtree');
+const uuid = require('node-uuid');
 /** Express **/
 var app = express();
-
+var fs = require("fs");
 /** Database Connection **/
 var env = app.get('env');
 if (env !== 'production' && env !== 'development' && env !== 'test') {
@@ -28,7 +32,8 @@ var User = require('./models/user');
 
 /** Authentication **/
 var auth = require('./middleware/auth');
-
+const Memtree = require('./middleware/treemanager');
+memt = new Memtree();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -42,8 +47,142 @@ app.use('/', require('./routes/index'));
 app.use('/init', require('./routes/init'));
 app.use('/login', require('./routes/login'));
 app.use('/token', require('./routes/token'));
+app.use('/users', require('./routes/users'));
+app.use('/files',require('./routes/files'));
 app.use('/authtest', require('./routes/authtest'));
 /** Routing Ends **/
+
+// app.use(auth.jwt(),function(req, res){
+//   var pathname = url.parse(req.url).pathname;
+//   var realPath = "/mnt" + pathname;
+//   console.log(realPath);
+//   fs.exists(realPath, function (exists) {
+//     if (!exists) {
+//       console.log('111');
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.write("This request URL " + pathname + " was not found on this server.");
+//       res.end();
+//     } 
+//     else {
+//       console.log('222');
+//       fs.readFile(realPath, "binary", function(err, file) {
+//           if (err) {
+//               res.writeHead(500, {'Content-Type': 'text/plain'});
+//               res.end(err);
+//           } else {
+//               res.writeHead(200, {'Content-Type': 'text/html'});
+//               res.write(file, "binary");
+//               res.end();
+//           }
+//        });
+//     }
+//   });
+// });
+
+var io = require("socket.io").listen(6969);
+dmap = new Map();
+mtree = new MTObj();
+
+io.sockets.on('connection', function(socket){
+  console.log("Scanner Connected!");
+  socket.on('addfoldernode', function(msg){
+    var memobj = new MTObj();
+    if(!memt.has(msg.uid)){
+      memobj.create(msg.uid,msg.type,msg.parent,[],msg.path,msg.readlist,msg.writelist,msg.owner,msg.createtime,msg.changetime,msg.modifytime,msg.accesstime,msg.size);
+      memt.add(msg.uid,memobj);
+      dmap.set(msg.path,msg.uid);
+    }
+  });
+
+  socket.on('addfilenode', function(msg){
+    if(!memt.has(msg.uid)){
+      var memobj = new MTObj();
+      memobj.create(msg.uid,msg.type,msg.parent,[],msg.path,msg.readlist,msg.writelist,msg.owner,msg.createtime,msg.changetime,msg.modifytime,msg.accesstime,msg.size);
+      memt.add(msg.uid,memobj);
+    }
+  });
+
+  socket.on('addchild', function(msg){
+    memt.addchild(msg.parent,memt.get(msg.uid));
+  });
+
+  socket.on('setroot', function(msg){
+    memt.setroot(msg.uid);
+  });
+
+  socket.on('deletefolderorfile', function(msg){
+    console.log('yes,we can');
+    memt.deletefilebypath(msg.path);
+  });
+});
+
+console.log('111');
+var spawn = require('child_process').spawn;
+spawn('node', ['/trynode/scanner.js']);
+console.log('222');
+// fmap = new Map();
+
+// var newlist = globby.sync(['/mnt/**']);
+// newlist.forEach(function(f){
+//   fstat=fs.statSync(f);
+
+//   try{
+//     xattr.getSync(f,'user.uuid');
+//   }
+//   catch(e)
+//   {
+//     console.log(e);
+//     xattr.setSync(f,'user.uuid',uuid.v4());
+//     xattr.setSync(f,'user.readlist','32b84070-238c-4c61-bf1d-ab851cbc1841');
+//     xattr.setSync(f,'user.writelist','');
+//     xattr.setSync(f,'user.owner','32b84070-238c-4c61-bf1d-ab851cbc1841');
+//     if (fstat.isDirectory){
+//       xattr.setSync(f,'user.type','folder');
+//     }
+//     else{
+//       xattr.setSync(f,'user.type','file');
+//     }
+//   }
+//   var tuuid = xattr.getSync(f,'user.uuid').toString('utf-8');
+//   //console.log(tuuid);
+//   var treadlist = xattr.getSync(f,'user.readlist').toString('utf-8').split(',');
+//   var twritelist = xattr.getSync(f,'user.writelist').toString('utf-8').split(',');
+//   var towner = xattr.getSync(f,'user.owner').toString('utf-8').split(',');
+//   var ttype = xattr.getSync(f,'user.type').toString('utf-8');
+//   var tcreatetime = fstat.birthtime;
+//   var tchangetime = fstat.ctime;
+//   var tmodifytime = fstat.mtime;
+//   var taccesstime = fstat.atime;
+//   var tsize = fstat.size;
+//   if(dmap.has(f.substr(0,f.lastIndexOf('/')))){
+//     var tparent = dmap.get(f.substr(0,f.lastIndexOf('/')));
+//     memt.addchild(tparent,tuuid);
+//   }
+//   else{
+//     var tparent = '';
+//     memt.setroot(tuuid);
+//   }
+//   if (fstat&&fstat.isDirectory()){ 
+//     var memobj = new MTObj();
+//     var mpobj = new MTOpermission();
+//     var maobj = new MTOattribute();
+//     mpobj.create(treadlist,twritelist,towner);
+//     maobj.create(tcreatetime,tchangetime,tmodifytime,taccesstime,tsize,f.substr(f.lastIndexOf('/')+1));
+//     memobj.create(tuuid,ttype,tparent,[],f,treadlist,twritelist,towner,tcreatetime,tchangetime,tmodifytime,taccesstime,tsize);
+//     memt.add(tuuid,memobj);
+//     dmap.set(f,tuuid);
+//   }
+//   else{
+//     //console.log(xattr.getSync(f,'user.uuid').toString('utf-8'));
+//       var memobj = new MTObj();
+//       var mpobj = new MTOpermission();
+//       var maobj = new MTOattribute();
+//       mpobj.create(treadlist,twritelist,towner);
+//       maobj.create(tcreatetime,tchangetime,tmodifytime,taccesstime,tsize,f.substr(f.lastIndexOf('/')+1));
+//       memobj.create(tuuid,ttype,tparent,[],mpobj,maobj,f);
+//       memt.add(tuuid,memobj);
+//     }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ scanner.js
@@ -0,0 +1,116 @@
+const uuid = require('node-uuid');
+const globby = require('globby');
+var fs = require('fs');
+var xattr = require('fs-xattr');
+var schedule = require('node-schedule');
+var socket = require('socket.io-client')('http://localhost:6969');
+
+var dmap = new Map();
+
+var io = require("socket.io").listen(10086);
+
+console.log('333');
+
+function mtojson(){
+  this.uid='';
+  this.readlist=[];
+  this.writelist=[];
+  this.owner=[];
+  this.type='';
+  this.createtime='';
+  this.changetime='';
+  this.modifytime='';
+  this.accesstime='';
+  this.size='';
+  this.path='';
+  this.parent='';
+}
+
+io.sockets.on('connection', function(socket1){
+  socket1.on('deletefolderorfile', function(msg){
+    socket.emit('deletefolderorfile',msg);
+  });
+});
+
+
+var cronjob = function(){
+  newlist = globby.sync(['/mnt/**']);
+  newlist.forEach(function(f){
+    fstat=fs.statSync(f);
+    try{
+      xattr.getSync(f,'user.uuid');
+    }
+    catch(e)
+    {
+      xattr.setSync(f,'user.uuid',uuid.v4());
+      xattr.setSync(f,'user.readlist','');
+      xattr.setSync(f,'user.writelist','');
+      xattr.setSync(f,'user.owner','');
+      if (fstat&&fstat.isDirectory()){ 
+        xattr.setSync(f,'user.type','folder');
+      }
+      else if(fstat&&!fstat.isDirectory()){
+        xattr.setSync(f,'user.type','file');
+      }
+    }
+
+    var uid = xattr.getSync(f,'user.uuid').toString('utf-8');
+    var readlist = xattr.getSync(f,'user.readlist').toString('utf-8').split(',');
+    var writelist = xattr.getSync(f,'user.writelist').toString('utf-8').split(',');
+    var owner = xattr.getSync(f,'user.owner').toString('utf-8').split(',');
+    var type = xattr.getSync(f,'user.type').toString('utf-8');
+    var createtime = fstat.birthtime;
+    var changetime = fstat.ctime;
+    var modifytime = fstat.mtime;
+    var accesstime = fstat.atime;
+    var size = fstat.size;
+    var mtobj = new mtojson();
+    mtobj.uid=uid;
+    mtobj.readlist=readlist;
+    mtobj.writelist=writelist;
+    mtobj.owner=owner;
+    mtobj.type=type;
+    mtobj.createtime=createtime;
+    mtobj.changetime=changetime;
+    mtobj.modifytime=modifytime;
+    mtobj.accesstime=accesstime;
+    mtobj.size=size;
+    mtobj.path=f;
+
+    if(dmap.has(f.substr(0,f.lastIndexOf('/')))){
+      var parent = dmap.get(f.substr(0,f.lastIndexOf('/')));
+      mtobj.parent=parent;
+    }
+
+    if (fstat&&fstat.isDirectory()){ 
+      socket.emit('addfoldernode',mtobj);
+      dmap.set(f,uid);
+    }
+    else{
+      //console.log(xattr.getSync(f,'user.uuid').toString('utf-8'));
+      socket.emit('addfilenode',mtobj);
+    }
+
+    if(dmap.has(f.substr(0,f.lastIndexOf('/')))){
+      socket.emit('addchild',mtobj);
+    }
+    else{
+      socket.emit('setroot',mtobj);
+    }
+});
+}
+
+cronjob();
+
+socket.on('connect', function () { console.log("socket connected"); });
+
+
+var rule = new schedule.RecurrenceRule();
+// rule.dayOfWeek = [0, new schedule.Range(1, 6)];
+// rule.hour = 6;
+// rule.minute =0;
+rule.second = 0;
+schedule.scheduleJob(rule, function(){
+  cronjob();
+});
+
