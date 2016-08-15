c122001a7cfe0dcf04f735fe01c43f74e32fe704
lewis ma <lewis.ma@winsuntech.cn>
2016-08-15 11:35:49 +0800

    add async version of createDrive

--- src/lib/drive.js
+++ src/lib/drive.js
@@ -1,5 +1,7 @@
 import path from 'path'
+import fs from 'fs'
 
+import Promise from 'bluebird'
 import rimraf from 'rimraf'
 
 import { readXstat } from './xstat'
@@ -132,5 +134,7 @@ const createDrive = (target, callback) => {
   })  
 }  
 
-export { createDrive }
+const createDriveAsync = Promise.promisify(createDrive)
+
+export { createDrive, createDriveAsync }
 
