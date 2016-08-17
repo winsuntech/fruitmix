690f26fa0c67aca02075ae6acde2fa9a451d8c61
panda <wangpanhn@qq.com>
2016-08-17 17:32:56 +0800

    modify testXstats.js

--- test/lib/testXstats.js
+++ test/lib/testXstats.js
@@ -18,17 +18,6 @@ import {
 const debug = true;
 const expect = chai.expect;
 const FRUITMIX = 'user.fruitmix';
-// const uuidArr = [
-// 	'55658e02-a808-458d-8634-3e06e8133c08',
-// 	'a9908696-317f-43d4-bbaf-b4dd802121d5',
-// 	'85e0f699-1566-40a6-8d98-36a0353cd86f',
-// 	'5c4e8196-0d3f-4730-a949-4c9618d3505e',
-// 	'f03492f8-81d3-4223-94c9-c8768dd9f214',
-// 	'58606e3c-bbbc-4eae-b65a-fabbc7caf816',
-// 	'5fce974c-e0f6-4903-8a31-cf6c983b6855',
-// 	'2cc82128-a785-421a-a362-8b137d666aa0',
-// 	'96c90a58-6456-4e70-89b9-248032c4ebaa'
-// ];
 const uuidArr = [
 	UUID.v4(),
 	UUID.v4(),
@@ -44,8 +33,10 @@ const isUUID = (uuid) => (typeof uuid === 'string') ? validator.isUUID(uuid) : f
 describe('xstat.js', function(){
 
 	let cwd = process.cwd();
-	let tmpFoder = 'tmptest';
+	let tmpFoder = 'tmpFoder';
+	let tmpFile = 'tmpFile.js';
 	let fpath = path.join(cwd, tmpFoder);
+	let ffpath = path.join(fpath, tmpFile);
 
 	describe('readTimeStamp', function(){
 		let timeStamp;
@@ -74,114 +65,496 @@ describe('xstat.js', function(){
 
 	describe('readXstat', function(){
 
-		const beforeTODO = (done, callback) => {
+		beforeEach(done => {
 			rimraf(tmpFoder, err => {
 				if (err) return done(err);
 				mkdirp(tmpFoder, err => {
 					if (err) return done(err);
-					callback();
-				})
-			})
-		};
-
-		it('should return null if the second argument is null', function(done){
-			beforeTODO(done, () => {
-				readXstat(fpath, null, function(err, attr){
-					if (err) { return done(err); }
-					expect(attr).to.be.null;
 					done();
 				});
+			});
+		});
+
+		after(function(){
+			rimraf(tmpFoder, err => {
+				if(err) throw new Error('delete testFoder failed');
+			});
+		});
+
+		it('should return null if the second argument is null', function(done){
+			readXstat(fpath, null, function(err, attr){
+				if (err) { return done(err); }
+				expect(attr).to.be.null;
+				done();
 			});
 		});
 
 		it('should return default object if xattr non-exist', function(done){
-			beforeTODO(done, () => {
-				readXstat(fpath, function(err, attr){
-					if (err) { return done(err); }
-					expect(isUUID(attr.uuid)).to.be.true;
-					expect(attr.isDirectory()).to.be.ture;
-					expect(attr.abspath).to.equal(fpath);
-					expect(attr.owner).to.be.a('array');
-					done();
-				});
+			readXstat(fpath, function(err, attr){
+				if (err) { return done(err); }
+				expect(isUUID(attr.uuid)).to.be.true;
+				expect(attr.isDirectory()).to.be.ture;
+				expect(attr.abspath).to.equal(fpath);
+				expect(attr.owner).to.be.a('array');
+				done();
 			});
 		});
 
 		it('should return preset object', function(done){
-			beforeTODO(done, () => {
-				xattr.set(fpath, FRUITMIX, JSON.stringify({
-					name: 'panda',
-					age : 23
-				}), function(err){
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				name: 'panda',
+				age : 23
+			}), function(err){
+				if (err) { return done(err); }
+				readXstat(fpath, function(err, attr){
 					if (err) { return done(err); }
-					readXstat(fpath, function(err, attr){
-						if (err) { return done(err); }
-						expect(attr.name).to.equal('panda');
-						expect(attr.age).to.equal(23);
-						expect(attr.isDirectory()).to.be.ture;
-						expect(attr.abspath).to.equal(fpath);
-						done();
-					});
+					expect(attr.name).to.equal('panda');
+					expect(attr.age).to.equal(23);
+					expect(attr.isDirectory()).to.be.ture;
+					expect(attr.abspath).to.equal(fpath);
+					done();
 				});
 			});
 		});
 
 		it('should return error if owner not provided', function(done){
-			beforeTODO(done, () => {
-				readXstat(fpath, {}, function(err, attr){
-					expect(err).to.be.an('error');
+			readXstat(fpath, {}, function(err, attr){
+				expect(err).to.be.an('error');
+				done();
+			});
+		});
+
+		it('should return error if the second argument is not an object or undefind', function(done){
+			readXstat(fpath, 'handsome boy', (err, attr) => {
+				expect(err).to.be.an('error');
+				done();
+			});
+		});
+
+		it('should return a new uuid if preset uuid is a string', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: 'panda',
+				owner: [uuidArr[0]]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(isUUID(attr.uuid)).to.be.true;
+					expect(attr.owner).to.deep.equal([uuidArr[0]]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
 					done();
 				});
 			});
 		});
 
-		it('should return null if xattr is not valid json', function(done){
-			beforeTODO(done, () =>{
-				xattr.set(fpath, FRUITMIX, 'handsome boy', err => {
-					if (err) return done(err);
-					readXstat(fpath, (err, attr) => {
-						if(err) return done(err);
-						expect(attr).to.be.null;
-						done();
+		it('should return a new uuid if preset uuid is an object', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: { name: 'panda' },
+				owner: [uuidArr[0]]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(isUUID(attr.uuid)).to.be.true;
+					expect(attr.owner).to.deep.equal([uuidArr[0]]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return a new uuid if preset uuid is an array', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: [],
+				owner: [uuidArr[0]]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(isUUID(attr.uuid)).to.be.true;
+					expect(attr.owner).to.deep.equal([uuidArr[0]]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return preset uuid(uuid,owner)', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[1],
+				owner: [uuidArr[0]]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.uuid).to.deep.equal(uuidArr[1]);
+					expect(attr.owner).to.deep.equal([uuidArr[0]]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return empty array if perset owner is an object', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: { name: 'panda' }
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return empty array if perset owner is an undefined', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: undefined
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return empty array if perset owner is an uuid', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: uuidArr[0]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return empty array if perset owner is an empty array', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: []
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return empty array if perset owner is an object array', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: [{ name: 'panda' }]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return preset array', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: [uuidArr[1], uuidArr[2]]
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([uuidArr[1], uuidArr[2]]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return preset array without is not uuid', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: [uuidArr[1], uuidArr[2],'panda']
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.owner).to.be.deep.equal([uuidArr[1], uuidArr[2]]);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return undefined if cwd is a folder(because folder without hash attribute)', done => {
+			xattr.set(fpath, FRUITMIX, JSON.stringify({
+				uuid: uuidArr[0],
+				owner: [uuidArr[1]],
+				hash: sha256_1
+			}), err => {
+				if(err) return done(err);
+				readXstat(fpath, (err, attr) => {
+					if(err) return done(err);
+					expect(attr.uuid).to.be.deep.equal(uuidArr[0]);
+					expect(attr.owner).to.be.deep.equal([uuidArr[1]]);
+					expect(attr.writelist).to.be.an('undefined');
+					expect(attr.readlist).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					expect(attr.hash).to.be.an('undefined');
+					expect(attr.abspath).to.deep.equal(fpath);
+					done();
+				});
+			});
+		});
+
+		it('should return undefined if htime non-exist', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						hash: sha256_1
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.be.an('undefined');
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
 					});
 				});
 			});
 		});
 
-		it('should return error if the second argument is not an object or undefind', function(done){
-			beforeTODO(done, () => {
-				readXstat(fpath, 'handsome boy', (err, attr) => {
-					expect(err).to.be.an('error');
-					done();
-				});
-			})
-		});
-
-		it('should return preset owner', function(done){
-			rimraf(tmpFoder, err => {
-				if (err) return done(err);
-				mkdirp(tmpFoder, err => {
-					if (err) return done(err);
-					xattr.set(fpath, FRUITMIX, JSON.stringify({
-						age: 20
+		it('should return preset value', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						hash: sha256_1,
+						htime: stat.mtime.getTime()
 					}), err => {
-						if (err) return done(err);
-						let opt = {
-							owner : [uuidArr[0]],
-							writelist : [uuidArr[1]],
-							readlist : [uuidArr[2]]
-						};
-						readXstat(fpath, opt, (err, attr) => {
-							if (err) return done(err);
-							expect(attr.owner).to.be.equal(uuidArr[0]);
-							expect(attr.writelist).to.be.equal(uuidArr[1]);
-							expect(attr.readlist).to.be.equal(uuidArr[2]);
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.deep.equal(sha256_1);
+							expect(attr.htime).to.deep.equal(stat.mtime.getTime());
+							expect(attr.abspath).to.deep.equal(ffpath);
 							done();
 						});
-					})
-				})
-			})
+					});
+				});
+			});
 		});
+
+		it('should return undefined if hash value is a string', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						hash: 'panda',
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.be.an('undefined');
+							expect(attr.htime).to.be.an('undefined');
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should return undefined if hash value is an object', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						hash: { name: 'panda' },
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.be.an('undefined');
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should return undefined if hash value is an array', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						hash: [],
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.be.an('undefined');
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should return all preset value(uuid,owner,writelist,readlist,hash,htime,abspath)', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						writelist: [uuidArr[2]],
+						readlist: [uuidArr[3]],
+						hash: sha256_1,
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.deep.equal([uuidArr[2]]);
+							expect(attr.readlist).to.deep.equal([uuidArr[3]]);
+							expect(attr.hash).to.deep.equal(sha256_1);
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should return undefined if readlist is undefined', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						writelist: [uuidArr[2]],
+						hash: sha256_1,
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.deep.equal(sha256_1);
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should return undefined if writelist is undefined', done => {
+			fs.writeFile(ffpath, '', (err) => {
+				if(err) return done(err);
+				fs.stat(ffpath, (err,stat) => {
+					xattr.set(ffpath, FRUITMIX, JSON.stringify({
+						uuid: uuidArr[0],
+						owner: [uuidArr[1]],
+						readlist: [uuidArr[2]],
+						hash: sha256_1,
+						htime: stat.mtime.getTime()
+					}), err => {
+						if(err) done(err);
+						readXstat(ffpath, (err, attr) => {
+							expect(attr.uuid).to.deep.equal(uuidArr[0]);
+							expect(attr.owner).to.deep.equal([uuidArr[1]]);
+							expect(attr.writelist).to.be.an('undefined');
+							expect(attr.readlist).to.be.an('undefined');
+							expect(attr.hash).to.deep.equal(sha256_1);
+							expect(attr.abspath).to.deep.equal(ffpath);
+							done();
+						});
+					});
+				});
+			});
+		});
+
 
 		
 	});
